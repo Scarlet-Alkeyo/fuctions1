a69d997687e4a319ed3831fe28bc990162f3e362
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-03-25 08:22:32 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun main(){
+ var word= check("madam")
+ println(word)
+ var numbers= numbers(1,2,3)
+ var text = sentence("Banie bakes brown bagels and buns")
+ println(text)
+}
+
+
+//Q2
+fun numbers(num1:Int,num2:Int,num3:Int):Int{
+ var numbers= arrayOf(1,2,3)
+  return numbers(num1,num2,num3)
+  println(numbers.sum())
+ println(numbers.count())
+ println(numbers.average())
+}
+//Q4
+fun check(name:String):Boolean{
+ var ispalindrome:Boolean =true
+ return (ispalindrome)
+ println("madam")
+}
+//Q1
+fun sentence(word:String):Int{
+ var text = "Barnie bakes brown bagels and buns"
+   return
+}
+
+//q3
+
